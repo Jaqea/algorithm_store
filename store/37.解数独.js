364ae0f118c49037a9529fa38c656c2cba5a2777
Jaqea <1601676692@qq.com>
2023-09-06 08:50:24 +0800

    leetcode 37 ac

--- store/37.解数独.js
+++ store/37.解数独.js
@@ -13,18 +13,18 @@
 let res;
 
 const isVaild = (row, col, val, matrix) => {
-  for (let i = 0; i < 9; i++) {
+  for (let i = 0; i < matrix.length; i++) {
     if (val === matrix[i][col]) return false;
   }
 
-  for (let j = 0; j < 9; j++) if (val === matrix[row][j]) return false;
+  for (let j = 0; j < matrix.length; j++)
+    if (val === matrix[row][j]) return false;
 
-  const startRow = (row / 3) * 3;
-  const startCol = (col / 3) * 3;
+  const startRow = Math.floor(row / 3) * 3;
+  const startCol = Math.floor(col / 3) * 3;
 
-  for (let i = startRow; i < startRow + 3 && i < 9; i++) {
-    for (let j = startCol; j < startCol + 3 && j < 9; j++) {
-      // console.log(matrix[i][j], val, matrix[i][j] == val);
+  for (let i = startRow; i < startRow + 3; i++) {
+    for (let j = startCol; j < startCol + 3; j++) {
       if (matrix[i][j] === val) return false;
     }
   }
@@ -36,16 +36,14 @@ const trackback = (matrix) => {
   for (let i = 0; i < matrix.length; i++) {
     for (let j = 0; j < matrix[0].length; j++) {
       if (matrix[i][j] !== ".") continue;
-      else {
-        for (let k = 1; k <= 9; k++) {
-          if (isVaild(i, j, `${k}`, matrix)) {
-            matrix[i][j] = `${k}`;
-            if (trackback(matrix)) return true;
-            matrix[i][j] = ".";
-          }
+      for (let k = 1; k <= 9; k++) {
+        if (isVaild(i, j, `${k}`, matrix)) {
+          matrix[i][j] = `${k}`;
+          if (trackback(matrix)) return true;
+          matrix[i][j] = ".";
         }
-        return false;
       }
+      return false;
     }
   }
   return true;
