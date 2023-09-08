643ca9d5e41598ad8abf13672877e7dd8be690f3
Jaqea <1601676692@qq.com>
2023-09-08 09:34:33 +0800

    leetcode 55 ac

--- store/55.跳跃游戏.js
+++ store/55.跳跃游戏.js
@@ -9,5 +9,14 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function (nums) {};
+var canJump = function (nums) {
+  let cover = 0;
+  if (nums.length === 1) return true;
+
+  for (let i = 0; i <= cover; i++) {
+    cover = cover > nums[i] + i ? cover : nums[i] + i;
+    if (cover >= nums.length - 1) return true;
+  }
+  return false;
+};
 // @lc code=end
