d69d27215d1298cfbcc5a9e8095f85cd6dd4290c
Jaqea <1601676692@qq.com>
2023-09-22 09:40:46 +0800

    leetcode 509 70 ac

---
+++ store/509.斐波那契数.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function (n) {
+  let dp = [];
+  dp[0] = 0;
+  if (n === 0) return dp[0];
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[dp.length - 1];
+};
+// @lc code=end
