ae2a4f9f47fba38fac191389ab6bb4badb233379
Jaqea <1601676692@qq.com>
2023-10-04 10:35:04 +0800

    leetcode 377 ac

---
+++ store/377.组合总和-ⅳ.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=377 lang=javascript
+ *
+ * [377] 组合总和 Ⅳ
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var combinationSum4 = function (nums, target) {
+  let dp = new Array(target + 1).fill(0);
+  dp[0] = 1;
+
+  for (let j = 0; j < target + 1; j++) {
+    for (let i = 0; i < nums.length; i++) {
+      if (j - nums[i] >= 0) dp[j] += dp[j - nums[i]];
+    }
+  }
+
+  return dp[target];
+};
+// @lc code=end
