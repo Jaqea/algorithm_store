e37bdf5f0b7a32ceea655ceecce43e225bb6aa83
Jaqea <1601676692@qq.com>
2023-10-05 10:26:55 +0800

    leetcode 322 ac

---
+++ store/322.零钱兑换.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=322 lang=javascript
+ *
+ * [322] 零钱兑换
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  let dp = new Array(amount + 1).fill(Number.MAX_SAFE_INTEGER);
+  dp[0] = 0;
+
+  for (let i = 0; i < coins.length; i++) {
+    for (let j = coins[i]; j <= amount; j++) {
+      dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+    }
+  }
+
+  return dp[amount] !== Number.MAX_SAFE_INTEGER ? dp[amount] : -1;
+};
+// @lc code=end
