dd63a3594496ca55ce9c9a8c70757c205383c791
Jaqea <1601676692@qq.com>
2023-10-12 09:28:16 +0800

    leetcode 122 ac

--- store/122.买卖股票的最佳时机-ii.js
+++ store/122.买卖股票的最佳时机-ii.js
@@ -10,14 +10,24 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-  const dPrices = prices.map((val, i) => {
-    if (i + 1 < prices.length) return prices[i + 1] - val;
-  });
-  dPrices[dPrices.length - 1] = 0;
-  let sum = 0;
-  dPrices.forEach((val) => {
-    if (val > 0) sum += val;
-  });
-  return sum;
+  // 法一
+  // const dPrices = prices.map((val, i) => {
+  //   if (i + 1 < prices.length) return prices[i + 1] - val;
+  // });
+  // dPrices[dPrices.length - 1] = 0;
+  // let sum = 0;
+  // dPrices.forEach((val) => {
+  //   if (val > 0) sum += val;
+  // });
+  // return sum;
+  // 法二
+  let dp = [-prices[0], 0];
+
+  for (let i = 1; i < prices.length; i++) {
+    dp[0] = Math.max(dp[0], dp[1] - prices[i]);
+    dp[1] = Math.max(dp[1], dp[0] + prices[i]);
+  }
+
+  return dp[1];
 };
 // @lc code=end
---
+++ store/1223.掷骰子模拟.js
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=1223 lang=javascript
+ *
+ * [1223] 掷骰子模拟
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number[]} rollMax
+ * @return {number}
+ */
+var dieSimulator = function(n, rollMax) {
+
+};
+// @lc code=end
+
