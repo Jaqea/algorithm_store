06cf65bbb33a8b7f1ccc6dab372e6736a38461ca
Jaqea <1601676692@qq.com>
2023-10-20 09:08:40 +0800

    leetcode 674 ac

---
+++ store/674.最长连续递增序列.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=674 lang=javascript
+ *
+ * [674] 最长连续递增序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findLengthOfLCIS = function (nums) {
+  let dp = new Array(nums.length).fill(1),
+    max = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > nums[i - 1]) {
+      dp[i] = dp[i - 1] + 1;
+      max = max > dp[i] ? max : dp[i];
+    }
+  }
+
+  return max;
+};
+// @lc code=end
