362355cc3bf6c728c183e2e1323ad9b6985f6707
Jaqea <1601676692@qq.com>
2023-10-24 08:57:58 +0800

    leetcode 1143 ac

--- store/1143.最长公共子序列.js
+++ store/1143.最长公共子序列.js
@@ -17,9 +17,11 @@ var longestCommonSubsequence = function (text1, text2) {
 
   for (let i = 1; i < text1.length + 1; i++) {
     for (let j = 1; j < text2.length + 1; j++) {
-      if (text1[i] === text2[j]) dp[i][j] = dp[i - 1][j - 1] + 1;
-      else dp[i][j] = Math.max();
+      if (text1[i - 1] === text2[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+      else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
     }
   }
+
+  return dp[text1.length][text2.length];
 };
 // @lc code=end
