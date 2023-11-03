0642244e27a56ba198c65c5f17875d5fc7341d55
Jaqea <1601676692@qq.com>
2023-11-03 09:23:23 +0800

    leetcode 739 ac

--- store/739.每日温度.js
+++ store/739.每日温度.js
@@ -12,17 +12,16 @@
 var dailyTemperatures = function (temperatures) {
   let stack = new Array(temperatures.length),
     top = 0,
-    res = [];
-  stack[top++] = temperatures[0];
+    res = new Array(temperatures.length).fill(0);
+  stack[top++] = 0;
   for (let i = 1, len = temperatures.length; i < len; i++) {
-    const item = stack[top - 1];
-    if (temperatures[i] > item) {
-      res.push(top);
+    while (temperatures[i] > temperatures[stack[top - 1]] && top !== 0) {
+      res[stack[top - 1]] = i - stack[top - 1];
       top--;
     }
-    stack[top++] = temperatures[i];
+    stack[top++] = i;
   }
 
-  console.log(res);
+  return res;
 };
 // @lc code=end
