7faa64e5a642b3b179c9a68c572cc11bbbc5a664
Jaqea <1601676692@qq.com>
2023-11-25 09:45:07 +0800

    leetcode 200 nac

---
+++ store/200.岛屿数量.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=200 lang=javascript
+ *
+ * [200] 岛屿数量
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+
+const dfs = () => {};
+
+let dir;
+
+var numIslands = function (grid) {
+  dir = [0, 1, 0, -1, 0, 1, 0, -1];
+};
+// @lc code=end
