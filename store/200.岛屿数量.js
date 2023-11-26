c576927279267a0f1854f80e0ccf9f8b5799ff46
Jaqea <1601676692@qq.com>
2023-11-26 09:34:11 +0800

    leetcode 200 ac

--- store/200.岛屿数量.js
+++ store/200.岛屿数量.js
@@ -10,11 +10,48 @@
  * @return {number}
  */
 
-const dfs = () => {};
+const dfs = (grid, x, y) => {
+  for (let i = 0; i < dir[0].length; i++) {
+    const nextX = x + dir[0][i];
+    const nextY = y + dir[1][i];
 
-let dir;
+    if (
+      nextX >= 0 &&
+      nextX < grid.length &&
+      nextY >= 0 &&
+      nextY < grid[0].length &&
+      !vaildity[nextX][nextY] &&
+      grid[nextX][nextY] === "1"
+    ) {
+      vaildity[nextX][nextY] = 1;
+      dfs(grid, nextX, nextY);
+    }
+  }
+};
+
+let dir, vaildity;
 
 var numIslands = function (grid) {
-  dir = [0, 1, 0, -1, 0, 1, 0, -1];
+  // 法一：dfs
+  let result = 0;
+  dir = [
+    [0, 1, 0, -1],
+    [1, 0, -1, 0],
+  ];
+  vaildity = new Array(grid.length)
+    .fill()
+    .map(() => Array(grid[0].length).fill(0));
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (!vaildity[i][j] && grid[i][j] === "1") {
+        result++;
+        vaildity[i][j] = 1;
+        dfs(grid, i, j);
+      }
+    }
+  }
+
+  return result;
 };
 // @lc code=end
