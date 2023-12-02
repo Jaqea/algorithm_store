435adeba28643bd8efb201eb505f691a20011e06
Jaqea <1601676692@qq.com>
2023-12-02 09:26:46 +0800

    leetcode 130 ac

--- store/130.被围绕的区域.js
+++ store/130.被围绕的区域.js
@@ -10,6 +10,7 @@
  * @return {void} Do not return anything, modify board in-place instead.
  */
 const dfs = (board, x, y) => {
+  vaildity[x][y] = 1;
   for (let i = 0; i < 4; i++) {
     const nextX = x + dir[0][i];
     const nextY = y + dir[1][i];
@@ -19,7 +20,7 @@ const dfs = (board, x, y) => {
       nextX < board.length &&
       nextY < board[0].length &&
       !vaildity[nextX][nextY] &&
-      board === "O"
+      board[nextX][nextY] === "O"
     ) {
       vaildity[nextX][nextY] = 1;
       dfs(board, nextX, nextY);
@@ -39,9 +40,19 @@ var solve = function (board) {
   ];
 
   for (let i = 0; i < M; i++) {
-    for (let j = 0; j < N; j++) {
+    if (board[i][0] === "O") dfs(board, i, 0);
+    if (board[i][N - 1] === "O") dfs(board, i, N - 1);
+  }
+
+  for (let j = 0; j < N; j++) {
+    if (board[0][j] === "O") dfs(board, 0, j);
+    if (board[M - 1][j] === "O") dfs(board, M - 1, j);
+  }
+
+  for (let i = 1; i < M; i++) {
+    for (let j = 1; j < N; j++) {
       if (!vaildity[i][j] && board[i][j] === "O") {
-        dfs(board, i, j);
+        board[i][j] = "X";
       }
     }
   }
