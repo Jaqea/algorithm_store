7fa550df3a9cfeb6c40a9af4d8fb20946aa15374
Jaqea <1601676692@qq.com>
2023-12-03 09:35:25 +0800

    leetcode 417 ac

--- store/417.太平洋大西洋水流问题.js
+++ store/417.太平洋大西洋水流问题.js
@@ -10,7 +10,9 @@
  * @return {number[][]}
  */
 
-const dfs = (heights, x, y) => {
+const dfs = (heights, vaildity, x, y) => {
+  // if (vaildity[x][y]) return;
+  vaildity[x][y] = 1;
   for (let i = 0; i < 4; i++) {
     const nextX = x + dir[0][i];
     const nextY = y + dir[1][i];
@@ -19,29 +21,43 @@ const dfs = (heights, x, y) => {
       nextY >= 0 &&
       nextX < heights.length &&
       nextY < heights[0].length &&
-      !vaildity[nextX][nextY]
+      !vaildity[nextX][nextY] &&
+      heights[x][y] <= heights[nextX][nextY]
     ) {
-      vaildity[nextX][nextY] = 1;
-      dfs(heights, nextX, nextY);
+      dfs(heights, vaildity, nextX, nextY);
     }
   }
 };
 
-let vaildity, dir;
+let dir;
 
 var pacificAtlantic = function (heights) {
+  const res = [];
   const M = heights.length,
     N = heights[0].length;
-  vaildity = new Array(M).fill().map(() => Array(N).fill(0));
+  let pacific = new Array(M).fill().map(() => Array(N).fill(0));
+  let atlantic = new Array(M).fill().map(() => Array(N).fill(0));
   dir = [
     [-1, 0, 1, 0],
     [0, 1, 0, -1],
   ];
 
   for (let i = 0; i < M; i++) {
+    dfs(heights, pacific, i, 0);
+    dfs(heights, atlantic, i, N - 1);
+  }
+
+  for (let j = 0; j < N; j++) {
+    dfs(heights, pacific, 0, j);
+    dfs(heights, atlantic, M - 1, j);
+  }
+
+  for (let i = 0; i < M; i++) {
     for (let j = 0; j < N; j++) {
-      if (!vaildity[i][j]) dfs(heights, i, j);
+      if (pacific[i][j] && atlantic[i][j]) res.push([i, j]);
     }
   }
+
+  return res;
 };
 // @lc code=end
