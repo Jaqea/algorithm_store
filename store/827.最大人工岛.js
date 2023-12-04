1dad8d4134ca6082a254cfb17c322839902322aa
Jaqea <1601676692@qq.com>
2023-12-04 09:26:02 +0800

    leetcode 827 nac

--- store/827.最大人工岛.js
+++ store/827.最大人工岛.js
@@ -9,7 +9,8 @@
  * @param {number[][]} grid
  * @return {number}
  */
-const dfs = (grid, x, y) => {
+const dfs = (grid, x, y, mark) => {
+  s++;
   for (let i = 0; i < 4; i++) {
     const nextX = x + dir[0][i];
     const nextY = y + dir[1][i];
@@ -18,15 +19,17 @@ const dfs = (grid, x, y) => {
       nextX >= 0 &&
       nextY >= 0 &&
       nextX < grid.length &&
-      nextY < grid[0].length
+      nextY < grid[0].length &&
+      !vaildity[nextX][nextY] &&
+      grid[nextX][nextY] === 1
     ) {
-      vaildity[nextX][nextY] = 1;
-      dfs(grid, nextX, nextY);
+      vaildity[nextX][nextY] = mark;
+      dfs(grid, nextX, nextY, mark);
     }
   }
 };
 
-let vaildity, dir;
+let vaildity, dir, s;
 
 var largestIsland = function (grid) {
   dir = [
@@ -37,12 +40,30 @@ var largestIsland = function (grid) {
     .fill()
     .map(() => Array(grid[0].length).fill(0));
 
+  const map = new Map();
+  let mark = 1,
+    res = 0;
+
   for (let i = 0; i < grid.length; i++) {
     for (let j = 0; j < grid[0].length; j++) {
-      if (!vaildity[i][j]) {
-        dfs(grid, i, j);
+      if (!vaildity[i][j] && grid[i][j] === 1) {
+        vaildity[i][j] = mark;
+        s = 0;
+        dfs(grid, i, j, mark);
+        map.set(mark, s);
+        mark++;
       }
     }
   }
+
+  for (let value in map.entries()) res = Math.max(res, value);
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
+      if (grid[i][j] === 0 && !vaildity[i][j]) {
+      }
+    }
+  }
+  console.log(vaildity);
 };
 // @lc code=end
