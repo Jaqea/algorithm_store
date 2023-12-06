d44d54ffabd3589b69471af177890047eb845eb7
Jaqea <1601676692@qq.com>
2023-12-06 09:32:47 +0800

    leetcode 127 nac

--- store/127.单词接龙.js
+++ store/127.单词接龙.js
@@ -11,8 +11,43 @@
  * @param {string[]} wordList
  * @return {number}
  */
-var ladderLength = function(beginWord, endWord, wordList) {
+let map;
 
+const compare = (str1, str2) => {
+  let num = 0;
+  for (let i = 0; i < str2.length; i++) {
+    if (str1[i] === str2[i]) num++;
+  }
+
+  return num === 2;
+};
+
+const findPath = (str, wordList) => {
+  const path = [];
+  for (let i of wordList) {
+    if (compare(str, i) && !map.has(i)) path.push(i);
+  }
+  // console.log(path);
+  return path;
+};
+
+const bfs = (beginWord, endWord, wordList) => {
+  const queue = [];
+  let num = (front = rear = 0);
+  queue[rear++] = beginWord;
+  while (front !== rear) {
+    const item = queue[front++];
+    if (!map.has(item)) map.set(item, 1);
+    num++;
+    if (item === endWord) return num;
+    const paths = findPath(item, wordList);
+    for (let i of paths) queue[rear++] = i;
+    console.log(item, paths, queue.slice(front));
+  }
+};
+
+var ladderLength = function (beginWord, endWord, wordList) {
+  map = new Map();
+  return bfs(beginWord, endWord, wordList);
 };
 // @lc code=end
-
