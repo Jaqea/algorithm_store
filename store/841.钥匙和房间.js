8f63345d37fbcf06ec30e95d3e072ebbc79e6bd8
Jaqea <1601676692@qq.com>
2023-12-09 09:15:58 +0800

    leetcode 841 ac

--- store/841.钥匙和房间.js
+++ store/841.钥匙和房间.js
@@ -16,12 +16,15 @@ const dfs = (rooms, index) => {
   vaildity[index] = 1;
   const path = rooms[index];
   for (let i of path) {
+    if (!map.has(i)) map.set(i, 1);
+    else continue;
     dfs(rooms, i);
   }
 };
 
 var canVisitAllRooms = function (rooms) {
   const N = rooms.length;
+  map = new Map();
   vaildity = new Array(N).fill(0);
   dfs(rooms, 0);
   return vaildity.indexOf(0) === -1;
