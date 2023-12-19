21dd2686416ceb2c22eb1f4c3c59109f2ccc928a
Jaqea <1601676692@qq.com>
2023-12-19 09:30:53 +0800

    leetcode 1207 ac

--- store/1207.独一无二的出现次数.js
+++ store/1207.独一无二的出现次数.js
@@ -10,16 +10,29 @@
  * @return {boolean}
  */
 var uniqueOccurrences = function (arr) {
-  let hash = new Array(2000 + 2).fill(0),
-    sameHash = new Array(999 + 2).fill(0);
+  // let hash = new Array(2000 + 2).fill(0),
+  //   sameHash = new Array(999 + 2).fill(0);
+  // arr.forEach((item) => {
+  //   hash[item + 1000]++;
+  // });
+  // hash.forEach((item) => {
+  //   if (item > 0) sameHash[item]++;
+  // });
+  // return sameHash.every((item) => {
+  //   if (item <= 1) return true;
+  // });
+
+  const map = new Map(),
+    countMap = new Map();
   arr.forEach((item) => {
-    hash[item + 1000]++;
+    if (!map.has(item)) {
+      map.set(item, 0);
+    } else map.set(item, map.get(item) + 1);
   });
-  hash.forEach((item) => {
-    if (item > 0) sameHash[item]++;
-  });
-  return sameHash.every((item) => {
-    if (item <= 1) return true;
-  });
+  for (let value of map.values()) {
+    if (!countMap.has(value)) countMap.set(value, 1);
+    else return false;
+  }
+  return true;
 };
 // @lc code=end
