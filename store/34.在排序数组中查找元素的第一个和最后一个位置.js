9c700df84f8b1fa3d4db305256c5dbae246db70a
Jaqea <1601676692@qq.com>
2023-12-23 09:58:29 +0800

    leetcode 922 ac

--- store/34.在排序数组中查找元素的第一个和最后一个位置.js
+++ store/34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -10,19 +10,19 @@
  * @param {number} target
  * @return {number[]}
  */
-const leftVal = (nums, target) => {
-  let left, right, mid;
-  left = 0;
-  right = nums.length;
-  while (left < right) {
-    mid = left + Math.floor((right - left) / 2);
-    if (nums[mid] === target) right = mid;
-    else if (nums[mid] > target) right = mid;
-    else if (nums[mid] < target) left = mid + 1;
-  }
-  if (left === nums.length) return -1;
-  return nums[left] === target ? left : -1;
-};
+// const leftVal = (nums, target) => {
+//   let left, right, mid;
+//   left = 0;
+//   right = nums.length;
+//   while (left < right) {
+//     mid = left + Math.floor((right - left) / 2);
+//     if (nums[mid] === target) right = mid;
+//     else if (nums[mid] > target) right = mid;
+//     else if (nums[mid] < target) left = mid + 1;
+//   }
+//   if (left === nums.length) return -1;
+//   return nums[left] === target ? left : -1;
+// };
 const rightVal = (nums, target) => {
   // let left, right, mid;
   // left = 0;
@@ -36,19 +36,30 @@ const rightVal = (nums, target) => {
   // if (left - 1 < 0) return -1;
   // return nums[left - 1] === target ? left - 1 : -1;
 
+  //   let left, right, mid;
+  //   left = 0;
+  //   right = nums.length;
+  //   while (left < right) {
+  //     mid = left + Math.floor((right - left) / 2);
+  //     if (nums[mid] === target) left = mid + 1;
+  //     else if (nums[mid] > target) right = mid;
+  //     else if (nums[mid] < target) left = mid + 1;
+  //   }
+  //   if (left - 1 < 0) return -1;
+  //   return nums[left - 1] === target ? left - 1 : -1;
+  // };
+  // var searchRange = function (nums, target) {
+  //   return [leftVal(nums, target), rightVal(nums, target)];
+  // };
+
   let left, right, mid;
-  left = 0;
-  right = nums.length;
-  while (left < right) {
-    mid = left + Math.floor((right - left) / 2);
-    if (nums[mid] === target) left = mid + 1;
-    else if (nums[mid] > target) right = mid;
+  (left = 0), (right = nums.length - 1);
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] > target) right = mid - 1;
     else if (nums[mid] < target) left = mid + 1;
+    else break;
   }
-  if (left - 1 < 0) return -1;
-  return nums[left - 1] === target ? left - 1 : -1;
-};
-var searchRange = function (nums, target) {
-  return [leftVal(nums, target), rightVal(nums, target)];
+  console.log(left, right);
 };
 // @lc code=end
--- store/922.按奇偶排序数组-ii.js
+++ store/922.按奇偶排序数组-ii.js
@@ -10,16 +10,31 @@
  * @return {number[]}
  */
 var sortArrayByParityII = function (nums) {
-  let i, j, temp;
-  j = 1;
-  for (i = 0; i < nums.length; i += 2) {
-    if (nums[i] % 2 == 1) {
-      while (j < nums.length && nums[j] % 2 != 0) j += 2;
-      if (j < nums.length) {
-        temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-      }
+  // let i, j, temp;
+  // j = 1;
+  // for (i = 0; i < nums.length; i += 2) {
+  //   if (nums[i] % 2 == 1) {
+  //     while (j < nums.length && nums[j] % 2 != 0) j += 2;
+  //     if (j < nums.length) {
+  //       temp = nums[i];
+  //       nums[i] = nums[j];
+  //       nums[j] = temp;
+  //     }
+  //   }
+  // }
+  // return nums;
+
+  let odd, even;
+  (even = 0), (odd = 1);
+  while (even < nums.length && odd < nums.length) {
+    if (nums[even] % 2 === 0) even += 2;
+    if (nums[odd] % 2 !== 0) odd += 2;
+    if (nums[even] % 2 && nums[odd] % 2 === 0) {
+      const temp = nums[even];
+      nums[even] = nums[odd];
+      nums[odd] = temp;
+      even += 2;
+      odd += 2;
     }
   }
   return nums;
