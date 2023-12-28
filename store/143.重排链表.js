c22bf41c16a480c6917be7782522bf92b146cda4
Jaqea <1601676692@qq.com>
2023-12-28 09:12:51 +0800

    leetcode 143 nac

--- store/143.重排链表.js
+++ store/143.重排链表.js
@@ -17,34 +17,31 @@
  * @return {void} Do not return anything, modify head in-place instead.
  */
 var reorderList = function (head) {
-  if (head === null || head.next === null) return head;
-  let queue = [],
-    p = head.next,
-    q,
-    count = 0,
-    len = 0;
-
-  while (p != null) {
-    q = p.next;
-    p.next = null;
-    queue.push(p);
-    len++;
-    p = q;
-  }
-
-  p = head;
-  while (count < len) {
-    if (count % 2 === 0) {
-      p.next = queue[queue.length - 1];
-      queue.pop();
-    } else {
-      p.next = queue[0];
-      queue.shift();
-    }
-    p = p.next;
-    count++;
-  }
-
-  return head;
+  // if (head === null || head.next === null) return head;
+  // let queue = [],
+  //   p = head.next,
+  //   q,
+  //   count = 0,
+  //   len = 0;
+  // while (p != null) {
+  //   q = p.next;
+  //   p.next = null;
+  //   queue.push(p);
+  //   len++;
+  //   p = q;
+  // }
+  // p = head;
+  // while (count < len) {
+  //   if (count % 2 === 0) {
+  //     p.next = queue[queue.length - 1];
+  //     queue.pop();
+  //   } else {
+  //     p.next = queue[0];
+  //     queue.shift();
+  //   }
+  //   p = p.next;
+  //   count++;
+  // }
+  // return head;
 };
 // @lc code=end
