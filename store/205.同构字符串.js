2271c5e7fe1c8e7e499c9e79dc722e06ae76fb86
Jaqea <1601676692@qq.com>
2024-01-01 09:38:13 +0800

    leetcode 205 ac

--- store/205.同构字符串.js
+++ store/205.同构字符串.js
@@ -11,20 +11,33 @@
  * @return {boolean}
  */
 var isIsomorphic = function (s, t) {
+  // let mapS = new Map(),
+  //   mapT = new Map(),
+  //   res = true;
+  // s.split("").forEach((item, index) => {
+  //   if (mapS.has(item) && mapS.get(item) !== t[index]) res = false;
+  //   mapS.set(item, t[index]);
+  // });
+  // t.split("").forEach((item, index) => {
+  //   if (mapT.has(item) && mapT.get(item) !== s[index]) res = false;
+  //   mapT.set(item, s[index]);
+  // });
+
+  // if (mapS.size !== mapT.size) res = false;
+  // for (let key of mapS.keys()) if (key !== mapT.get(mapS.get(key))) res = false;
+  // return res;
+
   let mapS = new Map(),
     mapT = new Map(),
-    res = true;
-  s.split("").forEach((item, index) => {
-    if (mapS.has(item) && mapS.get(item) !== t[index]) res = false;
-    mapS.set(item, t[index]);
+    temp = "";
+  [].slice.call(s).forEach((e, i) => {
+    if (!mapS.has(e) && !mapT.has(t[i])) {
+      mapS.set(e, t[i]);
+      mapT.set(t[i], 1);
+    }
   });
-  t.split("").forEach((item, index) => {
-    if (mapT.has(item) && mapT.get(item) !== s[index]) res = false;
-    mapT.set(item, s[index]);
-  });
-
-  if (mapS.size !== mapT.size) res = false;
-  for (let key of mapS.keys()) if (key !== mapT.get(mapS.get(key))) res = false;
-  return res;
+  for (let str of s) temp = temp.concat(mapS.get(str));
+  if (temp === t) return true;
+  return false;
 };
 // @lc code=end
