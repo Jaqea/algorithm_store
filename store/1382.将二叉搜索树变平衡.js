282704194d61512f773940cc3ca091a09c8e39cd
Jaqea <1601676692@qq.com>
2024-01-07 09:42:37 +0800

    leetcode 1382 ac

--- store/1382.将二叉搜索树变平衡.js
+++ store/1382.将二叉搜索树变平衡.js
@@ -17,27 +17,45 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-let nodes;
-let traverse = (node) => {
-  if (!node) return;
-  traverse(node.left);
-  nodes.push(node.val);
-  traverse(node.right);
-};
+// let nodes;
+// let traverse = (node) => {
+//   if (!node) return;
+//   traverse(node.left);
+//   nodes.push(node.val);
+//   traverse(node.right);
+// };
 
-let create = (nodes, left, right) => {
-  if (left > right) return null;
-  let mid, node;
-  mid = Math.floor((left + right) / 2);
-  node = new TreeNode(nodes[mid]);
-  node.left = create(nodes, left, mid - 1);
-  node.right = create(nodes, mid + 1, right);
-  return node;
-};
+// let create = (nodes, left, right) => {
+//   if (left > right) return null;
+//   let mid, node;
+//   mid = Math.floor((left + right) / 2);
+//   node = new TreeNode(nodes[mid]);
+//   node.left = create(nodes, left, mid - 1);
+//   node.right = create(nodes, mid + 1, right);
+//   return node;
+// };
 
 var balanceBST = function (root) {
-  nodes = [];
-  traverse(root);
-  return create(nodes, 0, nodes.length - 1);
+  // nodes = [];
+  // traverse(root);
+  // return create(nodes, 0, nodes.length - 1);
+  let nodes = [];
+  transform(root);
+  return create(0, nodes.length - 1);
+  function transform(node) {
+    if (!node) return;
+    transform(node.left);
+    nodes.push(node);
+    transform(node.right);
+  }
+  function create(left, right) {
+    if (left > right) return null;
+
+    const mid = Math.floor((right + left) / 2);
+    nodes[mid].right = create(mid + 1, right);
+    nodes[mid].left = create(left, mid - 1);
+
+    return nodes[mid];
+  }
 };
 // @lc code=end
