7cee7baa8dbbcab086e8bee73196b5fb197b5880
Jaqea <1601676692@qq.com>
2024-01-15 09:21:40 +0800

    leetcode 52 nac

---
+++ store/52.n皇后-ii.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=52 lang=javascript
+ *
+ * [52] N皇后 II
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+const traverse = (n) => {};
+
+var totalNQueens = function (n) {};
+// @lc code=end
