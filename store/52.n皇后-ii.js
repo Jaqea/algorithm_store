7afcdf705b6ff05c30d2be08cd71db4e5c8e143a
Jaqea <1601676692@qq.com>
2024-01-16 10:10:08 +0800

    leetcode 52 ac

--- store/52.n皇后-ii.js
+++ store/52.n皇后-ii.js
@@ -10,7 +10,39 @@
  * @return {number}
  */
 
-const traverse = (n) => {};
+var totalNQueens = function (n) {
+  let count = 0;
+  const chessboard = new Array(n).fill().map(() => Array(n).fill("."));
 
-var totalNQueens = function (n) {};
+  const isVaild = (row, col, chessboard, n) => {
+    for (let i = 0; i < row; i++) if (chessboard[i][col] === "Q") return false;
+
+    for (let i = row - 1, j = col - 1; i > -1 && j > -1; i--, j--)
+      if (chessboard[i][j] === "Q") return false;
+
+    for (let i = row - 1, j = col + 1; i > -1 && j < n; i--, j++)
+      if (chessboard[i][j] === "Q") return false;
+
+    return true;
+  };
+
+  const traverse = (n, row, chessboard) => {
+    if (row === n) {
+      count++;
+      return;
+    }
+
+    for (let col = 0; col < n; col++) {
+      if (isVaild(row, col, chessboard, n)) {
+        chessboard[row][col] = "Q";
+        traverse(n, row + 1, chessboard);
+        chessboard[row][col] = ".";
+      }
+    }
+  };
+
+  traverse(n, 0, chessboard);
+
+  return count;
+};
 // @lc code=end
