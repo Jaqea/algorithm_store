edd74fb05a5379c02f804cd84547d4be83baf26d
Jaqea <1601676692@qq.com>
2024-01-21 10:16:15 +0800

    leetcode 463 ac

--- store/463.岛屿的周长.js
+++ store/463.岛屿的周长.js
@@ -46,19 +46,76 @@ const dfs = (grid, x, y) => {
 };
 
 var islandPerimeter = function (grid) {
-  const M = grid.length,
-    N = grid[0].length;
-  vaildity = new Array(M).fill().map(() => Array(N).fill(0));
-  s = 0;
-  dir = [
-    [-1, 0, 1, 0],
-    [0, 1, 0, -1],
-  ];
-  for (let i = 0; i < M; i++) {
-    for (let j = 0; j < N; j++) {
+  // const M = grid.length,
+  //   N = grid[0].length;
+  // vaildity = new Array(M).fill().map(() => Array(N).fill(0));
+  // s = 0;
+  // dir = [
+  //   [-1, 0, 1, 0],
+  //   [0, 1, 0, -1],
+  // ];
+  // for (let i = 0; i < M; i++) {
+  //   for (let j = 0; j < N; j++) {
+  //     if (grid[i][j] === 1 && !vaildity[i][j]) {
+  //       vaildity[i][j] = 1;
+  //       dfs(grid, i, j);
+  //     }
+  //   }
+  // }
+
+  // return s;
+
+  let s = 0;
+  const vaildity = new Array(grid.length)
+      .fill()
+      .map(() => Array(grid[0].length).fill(0)),
+    dirs = [
+      [-1, 0],
+      [0, 1],
+      [1, 0],
+      [0, -1],
+    ];
+
+  const dfs = (x, y) => {
+    if (x === 0) s++;
+    if (y === 0) s++;
+
+    if (x === grid.length - 1) s++;
+
+    if (y === grid[0].length - 1) s++;
+
+    for (let dir of dirs) {
+      const nextX = x + dir[0];
+      const nextY = y + dir[1];
+
+      if (
+        nextX >= 0 &&
+        nextY >= 0 &&
+        nextX < grid.length &&
+        nextY < grid[0].length &&
+        !grid[nextX][nextY]
+      )
+        s++;
+
+      if (
+        nextX >= 0 &&
+        nextX < grid.length &&
+        nextY >= 0 &&
+        nextY < grid[0].length &&
+        !vaildity[nextX][nextY] &&
+        grid[nextX][nextY] === 1
+      ) {
+        vaildity[nextX][nextY] = 1;
+        dfs(nextX, nextY);
+      }
+    }
+  };
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[0].length; j++) {
       if (grid[i][j] === 1 && !vaildity[i][j]) {
         vaildity[i][j] = 1;
-        dfs(grid, i, j);
+        dfs(i, j);
       }
     }
   }
