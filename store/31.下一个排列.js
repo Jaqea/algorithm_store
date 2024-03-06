895d7811a23825f963f01218cf23990dcb61c780
Jaqea <1601676692@qq.com>
2024-03-06 09:52:47 +0800

    leetcode 31 nac

--- store/31.下一个排列.js
+++ store/31.下一个排列.js
@@ -10,6 +10,35 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var nextPermutation = function (nums) {
-  let temp;
+  let flag = 0,
+    i,
+    j;
+
+  loop: for (i = nums.length - 1; i > 0; i--) {
+    for (j = i - 1; j > -1; j--) {
+      if (nums[i] > nums[j]) {
+        let temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        flag = 1;
+        break loop;
+      }
+    }
+  }
+
+  if (flag) {
+    j++;
+    for (i = j; i < nums.length; i++) {
+      for (j = i; j < nums.length - i - 1; j++) {
+        if (nums[j] > nums[j + 1]) {
+          const temp = nums[j];
+          nums[j] = nums[j + 1];
+          nums[j + 1] = temp;
+        }
+      }
+    }
+  } else nums.reverse();
+
+  return nums;
 };
 // @lc code=end
