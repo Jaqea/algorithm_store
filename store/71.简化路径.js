34ac684f1c8b0856aada90b17fa083942e8e07ba
Jaqea <1601676692@qq.com>
2024-03-18 09:38:23 +0800

    leetcode 71 nac

--- store/71.简化路径.js
+++ store/71.简化路径.js
@@ -11,18 +11,42 @@
  */
 var simplifyPath = function (path) {
   const stack = [];
-  let top = 0;
+  let count = (top = 0);
   stack[top++] = path[0];
   for (let i = 1; i < path.length; i++) {
+    console.log(stack.slice(0, top), top);
     switch (path[i]) {
       case ".":
+        stack[top++] = ".";
+        count++;
         continue;
-        break;
-      case "..":
+      case "/":
+        if (count === 1) top--;
+        else if (count === 2) {
+          if (top === 3) top -= 2;
+          else if (top === 4) top -= 3;
+          else top -= 4;
+        }
+        count = 0;
+
+        if (stack[top - 1] === "/" || i === path.length - 1) continue;
+        else stack[top++] = path[i];
         break;
       default:
+        if (count === 1) top--;
+        else if (count === 2) {
+          if (top === 3) top -= 2;
+          else if (top === 4) top -= 3;
+          else top -= 4;
+        }
+        count = 0;
+        stack[top++] = path[i];
         break;
     }
   }
+
+  if (top > 1 && stack[top - 1] === "/") top--;
+
+  return stack.join("").slice(0, top);
 };
 // @lc code=end
