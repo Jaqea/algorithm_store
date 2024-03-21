83568c81e10e467e443a810b1c4facdefdb3b4c2
Jaqea <1601676692@qq.com>
2024-03-21 09:44:34 +0800

    leetcode 394 nac

---
+++ store/394.字符串解码.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=394 lang=javascript
+ *
+ * [394] 字符串解码
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function (s) {
+  const decode = (k, str) => {
+    if (!str.includes("[")) return str.repeat(k);
+    else {
+    }
+  };
+};
+// @lc code=end
