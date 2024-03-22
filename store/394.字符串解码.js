53bd0315052176c63dd426c2267e1608e3bc2254
Jaqea <1601676692@qq.com>
2024-03-22 09:45:12 +0800

    leetcode 394 nac

--- store/394.字符串解码.js
+++ store/394.字符串解码.js
@@ -10,10 +10,44 @@
  * @return {string}
  */
 var decodeString = function (s) {
-  const decode = (k, str) => {
+  let temp = [],
+    top = 0;
+  s = s.split("]").filter((item) => item);
+  s.forEach((item) => {
+    if (item[0] - "0") temp[top++] = item;
+    else {
+      temp[top++] = item.slice(0, item.indexOf("[") - 1);
+      temp[top++] = item.slice(item.indexOf("[") - 1);
+    }
+  });
+
+  s = temp;
+  temp = null;
+
+  const decode = (k = 1, str) => {
+    if (!str) return;
     if (!str.includes("[")) return str.repeat(k);
     else {
+      const index = str.indexOf("[");
+      const key = str[index - 1];
+      const value = str.slice(index + 1);
+      const res = str.slice(0, index - 1) + decode(key, value);
+      console.log(res);
+      return res.repeat(k);
     }
   };
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i][0] - "0") {
+      const k = s[i][0] - "0";
+      const value = s[i].slice(2);
+      console.log(k, value);
+      s[i] = decode(k, value);
+    }
+  }
+
+  console.log(s);
+
+  return s.join("");
 };
 // @lc code=end
