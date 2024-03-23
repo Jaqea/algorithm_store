10be9bd3a8baa34a44cfcea7fbe6831088cd2375
Jaqea <1601676692@qq.com>
2024-03-23 10:22:36 +0800

    leetcode 394 ac

--- store/394.字符串解码.js
+++ store/394.字符串解码.js
@@ -13,45 +13,21 @@ var decodeString = function (s) {
   const strStack = [],
     numStack = [];
   let top1 = (top2 = 0),
-    k = (value = res = "");
+    k = (value = "");
 
   for (let i = 0; i < s.length; i++) {
-    if (s[i] - "0") k += s[i];
+    if (s[i] >= "0" && s[i] <= "9") k += s[i];
     else if (s[i] === "[") {
-      numStack[top2++] = k - "0";
       strStack[top1++] = value;
+      numStack[top2++] = k - "0";
       k = value = "";
     } else if (s[i] === "]") {
+      const num = numStack[--top2];
+      const str = strStack[--top1];
+      value = str + value.repeat(num);
     } else value += s[i];
   }
 
-  s = temp;
-  temp = null;
-
-  const decode = (k = 1, str) => {
-    if (!str) return;
-    if (!str.includes("[")) return str.repeat(k);
-    else {
-      const index = str.indexOf("[");
-      const key = str[index - 1];
-      const value = str.slice(index + 1);
-      const res = str.slice(0, index - 1) + decode(key, value);
-      console.log(res);
-      return res.repeat(k);
-    }
-  };
-
-  for (let i = 0; i < s.length; i++) {
-    if (s[i][0] - "0") {
-      const k = s[i][0] - "0";
-      const value = s[i].slice(2);
-      console.log(k, value);
-      s[i] = decode(k, value);
-    }
-  }
-
-  console.log(s);
-
-  return s.join("");
+  return value;
 };
 // @lc code=end
