3ec2f8834eaec778830fc551f2e70190f3de3292
Jaqea <1601676692@qq.com>
2024-03-29 21:28:26 +0800

    leetcode 2 nac

--- store/2.两数相加.js
+++ store/2.两数相加.js
@@ -21,21 +21,49 @@ var addTwoNumbers = function (l1, l2) {
   let p,
     q,
     sign = 0,
+    len1 = (len2 = 1),
     num;
   p = l1;
   q = l2;
 
-  while (p && q) {
-    num = p.val + q.val + sign;
-    if (num > 9) {
-      sign = 1;
-      num %= 10;
-    }
-    q.val = num;
+  while (p) {
     p = p.next;
-    q = q.next;
+    len1++;
   }
 
-  return l2;
+  while (q) {
+    q = q.next;
+    len2++;
+  }
+
+  p = l1;
+  q = l2;
+  if (len1 > len2) {
+    while (p) {
+      if (q) num = p.val + q.val + sign;
+      else num = p.val + sign;
+      if (num > 9) {
+        sign = 1;
+        num %= 10;
+      }
+      p.val = num;
+      p = p.next;
+      if (q) q = q.next;
+    }
+  } else {
+    while (q) {
+      if (p) num = p.val + q.val + sign;
+      else num = q.val + sign;
+      if (num > 9) {
+        sign = 1;
+        num %= 10;
+      }
+      q.val = num;
+      q = q.next;
+      if (p) p = p.next;
+    }
+  }
+
+  return len1 > len2 ? l1 : l2;
 };
 // @lc code=end
