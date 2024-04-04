4d261f435c08b1b2e5f276eeccde6df00b6dee63
Jaqea <1601676692@qq.com>
2024-04-04 10:13:37 +0800

    leetcode 373 nac

--- store/373.查找和最小的-k-对数字.js
+++ store/373.查找和最小的-k-对数字.js
@@ -12,7 +12,21 @@
  * @return {number[][]}
  */
 var kSmallestPairs = function (nums1, nums2, k) {
-  
+  if (k === 1) return [nums1[0], nums2[0]];
+
+  const res = [];
+  let left = (right = 0);
+  res.push([nums1[left], nums2[right]]);
+  k--;
+
+  while (k) {
+    if (nums1[left + 1] + nums2[right] > nums1[left] + nums2[right + 1])
+      right++;
+    else left++;
+    res.push([nums1[left], nums2[right]]);
+    k--;
+  }
+
+  return res;
 };
 // @lc code=end
-
