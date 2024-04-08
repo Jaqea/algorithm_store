813ebbc01dd237dd27e4836665c40590512eff82
Jaqea <1601676692@qq.com>
2024-04-08 09:16:48 +0800

    leetcode 387 ac

---
+++ store/387.字符串中的第一个唯一字符.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=387 lang=javascript
+ *
+ * [387] 字符串中的第一个唯一字符
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var firstUniqChar = function (s) {
+  const map = new Map();
+  for (let i = 0; i < s.length; i++) {
+    if (!map.has(s[i])) {
+      map.set(s[i], i);
+    } else map.set(s[i], -1);
+  }
+
+  for (let value of map.values()) if (value !== -1) return value;
+
+  return -1;
+};
+// @lc code=end
