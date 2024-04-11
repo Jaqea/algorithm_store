2852e6b8f99667e7302024cd4ca0e24609965491
Jaqea <1601676692@qq.com>
2024-04-11 09:17:43 +0800

    leetcode 1248 tnac

--- store/1248.统计「优美子数组」.js
+++ store/1248.统计「优美子数组」.js
@@ -11,15 +11,26 @@
  * @return {number}
  */
 var numberOfSubarrays = function (nums, k) {
-  let res = (pre = 0);
-  const map = new Map();
-  map.set(pre, 1);
+  /** 法一：前缀和 */
+  // let res = (pre = 0);
+  // const map = new Map();
+  // map.set(pre, 1);
 
+  // for (let i = 0; i < nums.length; i++) {
+  //   if (nums[i] % 2) pre++;
+  //   if (map.has(pre - k)) res += map.get(pre - k);
+  //   if (!map.has(pre)) map.set(pre, 1);
+  //   else map.set(pre, map.get(pre) + 1);
+  // }
+
+  // return res;
+  /** 法二： */
+  let res = (count = l = r = 0);
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] % 2) pre++;
-    if (map.has(pre - k)) res += map.get(pre - k);
-    if (!map.has(pre)) map.set(pre, 1);
-    else map.set(pre, map.get(pre) + 1);
+    if (nums[i] % 2) count++;
+
+    if (count === k) {
+    }
   }
 
   return res;
