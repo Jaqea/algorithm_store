3d8d1694f7bb75833ec41fa891073296d0747219
Jaqea <1601676692@qq.com>
2024-04-13 09:51:21 +0800

    leetcode 146 nac

---
+++ store/146.lru-缓存.js
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=146 lang=javascript
+ *
+ * [146] LRU 缓存
+ */
+
+// @lc code=start
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function (capacity) {
+  this.vMap = new Map();
+  this.tMap = new Map();
+  this.time = 0;
+  this.capacity = capacity;
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function (key) {
+  if (this.vMap.has(key)) return this.vMap.get(key);
+  else return -1;
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function (key, value) {
+  if (this.capacity && !this.vMap.has(key)) {
+    this.vMap.set(key, value);
+    this.tMap.set(key, ++this.time);
+  } else if (!this.capacity) {
+  }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+// @lc code=end
