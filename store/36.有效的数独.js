1e3b89da367b3225c103c6d6c069afb25bcb6941
Jaqea <1601676692@qq.com>
2024-04-14 09:34:46 +0800

    leetcode 36 ac

--- store/36.有效的数独.js
+++ store/36.有效的数独.js
@@ -12,19 +12,39 @@
 var isValidSudoku = function (board) {
   const rowMap = new Array(board.length)
     .fill()
-    .map((i) => Array(board[0].length).fill("."));
+    .map((i) => Array(board[0].length));
   const colMap = new Array(board[0].length)
     .fill()
-    .map((i) => Array(board.length).fill("."));
-  const subMap = [];
+    .map((i) => Array(board.length));
+  const subMap = new Array(3).fill().map((i) =>
+    Array(3)
+      .fill()
+      .map((e) => Array(9))
+  );
 
   for (let row = 0; row < board.length; row++) {
     for (let col = 0; col < board[0].length; col++) {
       if (board[row][col] !== ".") {
-        if (!rowMap.has(board[row][col])) rowMap.set(board[row][col], [1]);
+        const index = board[row][col] - "0" - 1;
+
+        if (!rowMap[row][index]) {
+          rowMap[row][index] = 1;
+        } else return false;
+
+        if (!colMap[index][col]) {
+          colMap[index][col] = 1;
+        } else return false;
+
+        const rowIndex = Math.floor(row / 3);
+        const colIndex = Math.floor(col / 3);
+
+        if (!subMap[rowIndex][colIndex][index])
+          subMap[rowIndex][colIndex][index] = 1;
         else return false;
       }
     }
   }
+
+  return true;
 };
 // @lc code=end
