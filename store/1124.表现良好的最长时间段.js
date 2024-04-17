5a081fb9e11e15664a75303248064e5ec2eaa1d1
Jaqea <1601676692@qq.com>
2024-04-17 10:00:01 +0800

    leetcode 1124 ac

--- store/1124.表现良好的最长时间段.js
+++ store/1124.表现良好的最长时间段.js
@@ -11,9 +11,20 @@
  */
 var longestWPI = function (hours) {
   let max = (pre = 0);
+  const map = new Map();
   for (let i = 0; i < hours.length; i++) {
     if (hours[i] > 8) pre += 1;
     else pre -= 1;
+
+    if (pre > 0) max = i + 1;
+
+    if (!map.has(pre)) map.set(pre, i);
+    if (map.has(pre - 1)) {
+      const j = map.get(pre - 1);
+      max = Math.max(i - j, max);
+    }
   }
+
+  return max;
 };
 // @lc code=end
