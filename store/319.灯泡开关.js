e0c3b93264f8dd033933575c681c9f78eb73862c
Jaqea <1601676692@qq.com>
2024-04-20 09:44:36 +0800

    leetcode 319 ac

--- store/319.灯泡开关.js
+++ store/319.灯泡开关.js
@@ -10,27 +10,11 @@
  * @return {number}
  */
 var bulbSwitch = function (n) {
-  if (n === 0) return 0;
-  else if (n === 1) return n;
-  else if (n === 2) return Math.ceil(n / 2);
-  else {
-    let count = Math.ceil(n / 2);
-    const dp = new Array(n).fill(1);
-    for (let i = 1; i < n; i += 2) dp[i] = 0;
-
-    for (let i = 2; i < n; i++) {
-      for (let j = i; j < n; j += i + 1) {
-        if (dp[j]) {
-          dp[j] = 0;
-          count--;
-        } else {
-          dp[j] = 1;
-          count++;
-        }
-      }
-    }
-
-    return count;
+  let count = 0;
+  for (let i = 1; i <= n; i++) {
+    if (Math.sqrt(i) === parseInt(Math.sqrt(i))) count++;
   }
+
+  return count;
 };
 // @lc code=end
