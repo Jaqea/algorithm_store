1cd877cae5fe58381dcbb40645236420affff0e3
Jaqea <1601676692@qq.com>
2024-04-21 09:59:38 +0800

    leetcode 80 ac

--- store/80.删除有序数组中的重复项-ii.js
+++ store/80.删除有序数组中的重复项-ii.js
@@ -10,20 +10,11 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  const map = new Map();
   let slow = (fast = 0);
 
   while (fast < nums.length) {
-    if (!map.has(nums[fast])) map.set(nums[fast], 1);
-    else map.set(nums[fast], map.get(nums[fast]) + 1);
-
-    if (map.get(nums[fast]) > 2) {
-      while (nums[slow] === nums[fast]) fast++;
-      if (fast === nums.length) {
-        break;
-      }
-    }
-    nums[slow++] = nums[fast++];
+    if (slow >= 2 && nums[fast] === nums[slow - 2]) fast++;
+    else nums[slow++] = nums[fast++];
   }
 
   return slow;
