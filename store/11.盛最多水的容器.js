1878a66dadd634b161550bef0be1421c78304509
Jaqea <1601676692@qq.com>
2024-04-25 09:36:36 +0800

    leetcode 11 nac

---
+++ store/11.盛最多水的容器.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=javascript
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let max = 0;
+};
+// @lc code=end
