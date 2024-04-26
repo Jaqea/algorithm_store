f04bc7bb75c41c04de9dbfb8d1d9293fa22ead3c
Jaqea <1601676692@qq.com>
2024-04-26 09:22:29 +0800

    leetcode 11 ac

--- store/11.盛最多水的容器.js
+++ store/11.盛最多水的容器.js
@@ -10,6 +10,40 @@
  * @return {number}
  */
 var maxArea = function (height) {
-  let max = 0;
+  /** 暴力解法(超时) */
+  // let max = 0;
+  // for (let i = 0; i < height.length - 1; i++) {
+  //   let width = 0;
+  //   for (let j = i + 1; j < height.length; j++) {
+  //     if (height[j] >= height[i]) width = j - i;
+  //   }
+  //   max = max > width * height[i] ? max : width * height[i];
+  // }
+
+  // for (let i = height.length - 1; i > 0; i--) {
+  //   let width = 0;
+  //   for (let j = i - 1; j > -1; j--) {
+  //     if (height[j] >= height[i]) width = i - j;
+  //   }
+  //   max = max > width * height[i] ? max : width * height[i];
+  // }
+
+  // return max;
+  /**  */
+  let left = (max = 0),
+    right = height.length - 1;
+
+  while (left < right) {
+    const width = right - left;
+    const s =
+      height[left] > height[right]
+        ? width * height[right]
+        : width * height[left];
+    max = max > s ? max : s;
+    if (height[left] > height[right]) right--;
+    else left++;
+  }
+
+  return max;
 };
 // @lc code=end
