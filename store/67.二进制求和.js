d48be58b880b601786f093a12501feb3535127f7
Jaqea <1601676692@qq.com>
2024-05-10 10:51:38 +0800

    leetcode 67 ac

--- store/67.二进制求和.js
+++ store/67.二进制求和.js
@@ -10,8 +10,20 @@
  * @param {string} b
  * @return {string}
  */
-var addBinary = function(a, b) {
+var addBinary = function (a, b) {
+  let sum,
+    carry = 0,
+    res = "";
+  for (let i = a.length - 1, j = b.length - 1; i >= 0 || j >= 0; i--, j--) {
+    sum = carry;
+    sum += i >= 0 ? a[i] - "0" : 0;
+    sum += j >= 0 ? b[j] - "0" : 0;
+    res += (sum % 2) + "";
+    carry = Math.floor(sum / 2);
+  }
 
+  if (carry > 0) res += carry + "";
+
+  return res.split("").reverse().join("");
 };
 // @lc code=end
-
