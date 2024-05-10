101ce16923e5419b1b2a5fde7c387f006328186d
Jaqea <1601676692@qq.com>
2024-05-10 10:55:41 +0800

    leetcode 43 nac

---
+++ store/43.字符串相乘.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode.cn id=43 lang=javascript
+ *
+ * [43] 字符串相乘
+ */
+
+// @lc code=start
+/**
+ * @param {string} num1
+ * @param {string} num2
+ * @return {string}
+ */
+var multiply = function (num1, num2) {
+  let sum,
+    carry = 0,
+    res = "";
+  for (let j = num2.length - 1; j >= 0; j--) {
+    for (let i = num2.length - 1; i >= 0; i--) {
+      sum = carry;
+    }
+  }
+};
+// @lc code=end
