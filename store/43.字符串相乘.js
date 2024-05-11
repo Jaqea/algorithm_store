f6ff59d62c875cb8251a6af09b9a66dc90f19fe8
Jaqea <1601676692@qq.com>
2024-05-11 09:55:05 +0800

    leetcode 41 nac

--- store/43.字符串相乘.js
+++ store/43.字符串相乘.js
@@ -12,12 +12,33 @@
  */
 var multiply = function (num1, num2) {
   let sum,
-    carry = 0,
-    res = "";
+    carry,
+    str,
+    res = 0;
+
   for (let j = num2.length - 1; j >= 0; j--) {
-    for (let i = num2.length - 1; i >= 0; i--) {
+    carry = 0;
+    str = "";
+    for (let i = num1.length - 1; i >= 0; i--) {
       sum = carry;
+      sum += parseInt(num2[j]) * parseInt(num1[i]);
+      str += sum % 10;
+      carry = Math.floor(sum / 10);
     }
+
+    if (carry > 0) str += carry;
+
+    res +=
+      parseInt(str.split("").reverse().join("")) *
+      Math.pow(10, num2.length - 1 - j);
+
+    console.log(
+      res,
+      parseInt(str.split("").reverse().join("")) *
+        Math.pow(10, num2.length - 1 - j)
+    );
   }
+
+  return res + "";
 };
 // @lc code=end
