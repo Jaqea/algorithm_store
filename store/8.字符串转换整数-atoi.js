29db30257c06249e6329a4fa7b7f3a77a783cf4f
Jaqea <1601676692@qq.com>
2024-05-13 09:34:20 +0800

    leetcode 8 ac

--- store/8.字符串转换整数-atoi.js
+++ store/8.字符串转换整数-atoi.js
@@ -9,8 +9,27 @@
  * @param {string} s
  * @return {number}
  */
-var myAtoi = function(s) {
+var myAtoi = function (s) {
+  const MAX = Math.pow(2, 31) - 1,
+    MIN = -Math.pow(2, 31);
+  let sig, lastIndex, res;
 
+  s = s.trim();
+  sig = s[0];
+
+  if (sig === "-" || sig === "+") s = s.slice(1);
+
+  for (lastIndex = 0; lastIndex < s.length; lastIndex++) {
+    if (s[lastIndex] < "0" || s[lastIndex] > "9") break;
+  }
+  s = s.slice(0, lastIndex);
+
+  res = Number(s);
+  res = sig === "-" ? -res : res;
+
+  if (res >= MAX) res = MAX;
+  else if (res <= MIN) res = MIN;
+
+  return res;
 };
 // @lc code=end
-
