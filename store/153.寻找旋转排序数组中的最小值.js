e2656eb77361acbd4b3bd8fa9181632d52f1be63
Jaqea <1601676692@qq.com>
2024-05-14 10:01:11 +0800

    leetcode 153 ac

--- store/153.寻找旋转排序数组中的最小值.js
+++ store/153.寻找旋转排序数组中的最小值.js
@@ -9,8 +9,21 @@
  * @param {number[]} nums
  * @return {number}
  */
-var findMin = function(nums) {
+var findMin = function (nums) {
+  let left = 0,
+    right = nums.length - 1,
+    mid;
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
 
+    if (nums[mid] < nums[right]) right = mid;
+    else if (nums[mid] < nums[left]) left = mid;
+    else {
+      if (nums[right] < nums[left]) left = mid + 1;
+      else right = mid - 1;
+    }
+  }
+
+  return nums[left];
 };
 // @lc code=end
-
