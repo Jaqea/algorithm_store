fe8c756cf650a9bf1e428eea2e6abe253c1b0e76
Jaqea <1601676692@qq.com>
2024-05-16 10:12:40 +0800

    leetcode 81 nac

--- store/81.搜索旋转排序数组-ii.js
+++ store/81.搜索旋转排序数组-ii.js
@@ -10,8 +10,27 @@
  * @param {number} target
  * @return {boolean}
  */
-var search = function(nums, target) {
+var search = function (nums, target) {
+  let left = 0,
+    right = nums.length - 1,
+    mid;
 
+  while (left <= right) {
+    mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return true;
+    else if (target > nums[mid]) {
+      if (nums[mid] > nums[left]) left = mid + 1;
+      else if (target === nums[right]) return true;
+      else if (target > nums[right]) right = mid - 1;
+      else left = mid + 1;
+    } else if (target < nums[mid]) {
+      if (nums[mid] < nums[left]) left = mid + 1;
+      else if (target === nums[left]) return true;
+      else if (target > nums[left]) right = mid - 1;
+      else left = mid + 1;
+    }
+  }
+
+  return false;
 };
 // @lc code=end
-
