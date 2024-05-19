d29022dba4424dd406776dfae3802b2fe1048e72
Jaqea <1601676692@qq.com>
2024-05-19 10:03:24 +0800

    leetcode 34 ac

--- store/34.在排序数组中查找元素的第一个和最后一个位置.js
+++ store/34.在排序数组中查找元素的第一个和最后一个位置.js
@@ -10,75 +10,28 @@
  * @param {number} target
  * @return {number[]}
  */
-// const leftVal = (nums, target) => {
-//   let left, right, mid;
-//   left = 0;
-//   right = nums.length;
-//   while (left < right) {
-//     mid = left + Math.floor((right - left) / 2);
-//     if (nums[mid] === target) right = mid;
-//     else if (nums[mid] > target) right = mid;
-//     else if (nums[mid] < target) left = mid + 1;
-//   }
-//   if (left === nums.length) return -1;
-//   return nums[left] === target ? left : -1;
-// };
-const rightVal = (nums, target) => {
-  // let left, right, mid;
-  // left = 0;
-  // right = nums.length - 1;
-  // while (left <= right) {
-  //   mid = left + Math.floor((right - left) / 2);
-  //   if (nums[mid] === target) left = mid + 1;
-  //   else if (nums[mid] > target) right = mid - 1;
-  //   else if (nums[mid] < target) left = mid + 1;
-  // }
-  // if (left - 1 < 0) return -1;
-  // return nums[left - 1] === target ? left - 1 : -1;
-
-  //   let left, right, mid;
-  //   left = 0;
-  //   right = nums.length;
-  //   while (left < right) {
-  //     mid = left + Math.floor((right - left) / 2);
-  //     if (nums[mid] === target) left = mid + 1;
-  //     else if (nums[mid] > target) right = mid;
-  //     else if (nums[mid] < target) left = mid + 1;
-  //   }
-  //   if (left - 1 < 0) return -1;
-  //   return nums[left - 1] === target ? left - 1 : -1;
-  // };
-  // var searchRange = function (nums, target) {
-  //   return [leftVal(nums, target), rightVal(nums, target)];
-  // };
-
-  let left,
-    right,
-    leftBorder = (rightBorder = -1);
-  if (target > nums[nums.length - 1] || target < nums[0]) return [-1, -1];
-
-  (left = 0), (right = nums.length - 1);
-
+var searchRange = function (nums, target) {
+  const res = [-1, -1];
+  let left = 0,
+    right = nums.length - 1,
+    mid;
   while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > target) right = mid - 1;
-    else {
-      left = mid + 1;
-      rightBorder = left;
-    }
+    mid = Math.floor((left + right) / 2);
+    if (target === nums[mid]) {
+      res[0] = left;
+      res[1] = right;
+      break;
+    } else if (target > nums[mid]) left = mid + 1;
+    else right = mid - 1;
   }
 
-  (left = 0), (right = nums.length - 1);
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] < target) left = mid + 1;
-    else {
-      right = mid - 1;
-      leftBorder = right;
-    }
+  // console.log(res);
+
+  if (res[0] !== -1) {
+    while (nums[left] !== target) res[0] = ++left;
+    while (nums[right] !== target) res[1] = --right;
   }
-  if (leftBorder === -1 && rightBorder === -1) return [-1, -1];
-  if (rightBorder - leftBorder > 1) return [leftBorder + 1, rightBorder - 1];
-  return [-1, -1];
+
+  return res;
 };
 // @lc code=end
