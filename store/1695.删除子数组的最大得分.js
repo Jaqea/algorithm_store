aee7f0748b62d3a5e8fff3dcfe65f745de79350a
Jaqea <1601676692@qq.com>
2024-05-21 09:48:02 +0800

    leetcode 1695n ac

---
+++ store/1695.删除子数组的最大得分.js
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=1695 lang=javascript
+ *
+ * [1695] 删除子数组的最大得分
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumUniqueSubarray = function (nums) {};
+// @lc code=end
