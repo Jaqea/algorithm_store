0aca39440da3aa9289f11568ecc2f70fbb0c1031
Jaqea <1601676692@qq.com>
2024-05-24 09:47:22 +0800

    leetcode 148 ac

--- store/148.排序链表.js
+++ store/148.排序链表.js
@@ -16,5 +16,57 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var sortList = function (head) {};
+var sortList = function (head) {
+  /** 法一 */
+  // if (!head || !head.next) return head;
+  // let p = head,
+  //   root;
+  // const arr = [];
+  // while (p) {
+  //   arr.push(p.val);
+  //   p = p.next;
+  // }
+  // arr.sort((pre, next) => pre - next);
+  // root = new ListNode(arr[0]);
+  // p = root;
+  // for (let i = 1; i < arr.length; i++) {
+  //   p.next = new ListNode(arr[i]);
+  //   p = p.next;
+  // }
+  // return root;
+  /** 法二 */
+  if (!head || !head.next) return head;
+
+  let len = 0,
+    node = head,
+    dummyHead = new ListNode();
+  while (node) {
+    len++;
+    node = node.next;
+  }
+
+  for (let subLen = 1; subLen < len; subLen *= 2) {
+    let prev = dummyHead,
+      cur = dummyHead.next;
+    while (cur) {
+      const leftHead = cur;
+      for (let i = 1; i < subLen && cur.next; i++) cur = cur.next;
+      const rightHead = cur.next;
+      cur.next = rightHead;
+      for (let i = 1; i < subLen && cur && cur.next; i++) cur = cur.next;
+
+      let next = null;
+      if (cur) {
+        next = cur.next;
+        cur.next = null;
+      }
+
+      const merged = mergeTwoLists(leftHead, rightHead);
+      prev.next = merged;
+      while (prev.next) prev = prev.next;
+
+      cur = next;
+    }
+  }
+};
 // @lc code=end
