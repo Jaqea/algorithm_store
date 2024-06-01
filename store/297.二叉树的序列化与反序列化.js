30c471f8ff3c46387e5553f98d0938243425269a
Jaqea <1601676692@qq.com>
2024-06-01 10:13:28 +0800

    leetcode 297 ac

--- store/297.二叉树的序列化与反序列化.js
+++ store/297.二叉树的序列化与反序列化.js
@@ -20,6 +20,7 @@
  * @return {string}
  */
 var serialize = function (root) {
+  if (!root) return "";
   const queue = [],
     res = [];
   let front = (rear = 0);
@@ -27,7 +28,7 @@ var serialize = function (root) {
   queue[rear++] = root;
   while (front < rear) {
     const node = queue[front++];
-    if (node && node.val) {
+    if (node) {
       res.push(node.val);
     }
     if (!node) {
@@ -48,8 +49,8 @@ var serialize = function (root) {
       }
     }
   }
-  console.log(res);
-  return res.join;
+
+  return res.join("/");
 };
 
 /**
@@ -58,7 +59,34 @@ var serialize = function (root) {
  * @param {string} data
  * @return {TreeNode}
  */
-var deserialize = function (data) {};
+var deserialize = function (data) {
+  if (data.length) {
+    const res = data.split("/");
+    const queue = [];
+    let root = new TreeNode(res[0]),
+      front = (rear = 0),
+      index = 1;
+    queue[rear++] = root;
+    while (front < rear && index < res.length) {
+      const node = queue[front++];
+      node.left = res[index] ? new TreeNode(res[index]) : null;
+      node.right = res[index + 1] ? new TreeNode(res[index + 1]) : null;
+
+      if (node.left) {
+        queue[rear++] = node.left;
+      }
+
+      if (node.right) {
+        queue[rear++] = node.right;
+      }
+
+      index += 2;
+    }
+    return root;
+  }
+
+  return null;
+};
 
 /**
  * Your functions will be called as such:
