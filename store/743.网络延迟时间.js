48b0941140df2761f3c11a8721f04a670c073809
Jaqea <1601676692@qq.com>
2024-06-04 09:29:07 +0800

    leetcode 743 nac

--- store/743.网络延迟时间.js
+++ store/743.网络延迟时间.js
@@ -12,23 +12,9 @@
  * @return {number}
  */
 var networkDelayTime = function (times, n, k) {
-  const visited = new Array(n - 1).fill(0);
+  const visited = new Array(n).fill(0);
   let time = 0;
-  const dfs = (g, i) => {
-    if (g[i] && !visited[i]) {
-      visited[i] = 1;
-      time += g[i][2];
-      for (let j = 0; j < n; j++) {
-        if (g[j] && g[j][0] === g[i][0]) {
-          dfs(g, g[j][1] - 1);
-        }
-      }
-    }
-    return;
-  };
-  console.log(visited);
-  dfs(times, k - 1);
-  console.log(visited);
+
   return visited.includes(0) ? -1 : time;
 };
 // @lc code=end
