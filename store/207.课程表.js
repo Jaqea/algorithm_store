56a2a6c39d3f42b4c81936f165b441289cb9ab4e
Jaqea <1601676692@qq.com>
2024-06-09 10:07:27 +0800

    leetcode 207 ac

--- store/207.课程表.js
+++ store/207.课程表.js
@@ -12,23 +12,54 @@
  */
 var canFinish = function (numCourses, prerequisites) {
   const inDegree = new Map(),
-    map = new Map();
+    map = new Map(),
+    queue = [];
+  let front = (rear = 0);
 
   for (let i = 0; i < numCourses; i++) {
     inDegree.set(i, 0);
   }
 
   prerequisites.forEach((item) => {
-    const key = item[0],
-      value = item[1];
-    if (map.has(key)) {
-      map.get(key).push(value);
+    const first = item[0],
+      second = item[1];
+    if (map.has(second)) {
+      map.get(second).push(first);
     } else {
-      map.set(key, [value]);
+      map.set(second, [first]);
     }
-    inDegree.set(key, inDegree.get(key) + 1);
+    inDegree.set(first, inDegree.get(first) + 1);
   });
 
-  console.log(inDegree, map);
+  for (let [key, value] of inDegree.entries()) {
+    if (value === 0) {
+      queue[rear++] = key;
+    }
+  }
+
+  while (front < rear) {
+    const course = queue[front++];
+
+    if (!map.has(course)) {
+      continue;
+    }
+
+    const relates = map.get(course);
+    relates.forEach((item) => {
+      inDegree.set(item, inDegree.get(item) - 1);
+      if (inDegree.get(item) === 0) {
+        queue[rear++] = item;
+      }
+    });
+    map.delete(course);
+  }
+
+  for (let value of inDegree.values()) {
+    if (value !== 0) {
+      return false;
+    }
+  }
+
+  return true;
 };
 // @lc code=end
