2738d9320027aaf19f5fe6225754ef43a568d8c7
Jaqea <1601676692@qq.com>
2024-07-05 09:07:08 +0800

    leetcode 34 ac

--- store/35.搜索插入位置.js
+++ store/35.搜索插入位置.js
@@ -34,14 +34,29 @@ var searchInsert = function (nums, target) {
   // }
   // return left;
 
-  let left, right, mid;
-  (left = 0), (right = nums.length - 1);
+  // let left, right, mid;
+  // (left = 0), (right = nums.length - 1);
+  // while (left <= right) {
+  //   mid = Math.floor((right + left) / 2);
+  //   if (nums[mid] > target) right = mid - 1;
+  //   else if (nums[mid] < target) left = mid + 1;
+  //   else return mid;
+  // }
+  // return left;
+
+  let left = 0,
+    right = nums.length - 1;
   while (left <= right) {
-    mid = Math.floor((right + left) / 2);
-    if (nums[mid] > target) right = mid - 1;
-    else if (nums[mid] < target) left = mid + 1;
-    else return mid;
+    const mid = Math.floor((right + left) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
   }
+
   return left;
 };
 // @lc code=end
