5e57c5dfabb0c73920f10accdbe829a9ca445009
Jaqea <1601676692@qq.com>
2024-07-08 09:41:34 +0800

    leetcode 69 ac

--- store/69.x-的平方根.js
+++ store/69.x-的平方根.js
@@ -10,17 +10,33 @@
  * @return {number}
  */
 var mySqrt = function (x) {
-  if (x === 0 || x === 1) return x;
-  let left, right, mid;
-  left = 0;
-  right = x;
-  while (left < right) {
-    mid = left + Math.floor((right - left) / 2);
-    const sqr = mid * mid;
-    if (sqr == x) return mid;
-    else if (sqr < x) left = mid + 1;
-    else right = mid;
+  // if (x === 0 || x === 1) return x;
+  // let left, right, mid;
+  // left = 0;
+  // right = x;
+  // while (left < right) {
+  //   mid = left + Math.floor((right - left) / 2);
+  //   const sqr = mid * mid;
+  //   if (sqr == x) return mid;
+  //   else if (sqr < x) left = mid + 1;
+  //   else right = mid;
+  // }
+  // return left - 1;
+
+  let left = 0,
+    right = x;
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    const powMid = mid * mid;
+    if (powMid === x) {
+      return mid;
+    } else if (powMid > x) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
   }
+
   return left - 1;
 };
 // @lc code=end
