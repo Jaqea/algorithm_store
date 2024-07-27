85b9ac545d30be91343e8e5b5541d58f5117d5e0
Jaqea <1601676692@qq.com>
2024-07-27 13:31:02 +0800

    leetcode 236 ac

--- store/236.二叉树的最近公共祖先.js
+++ store/236.二叉树的最近公共祖先.js
@@ -28,6 +28,31 @@ const getAnc = (root, p, q) => {
   else return;
 };
 var lowestCommonAncestor = function (root, p, q) {
-  return getAnc(root, p, q);
+  // return getAnc(root, p, q);
+
+  const getAncestor = (node) => {
+    if (!node) {
+      return null;
+    }
+
+    if (node === p || node === q) {
+      return node;
+    }
+
+    const leftNode = getAncestor(node.left);
+    const rightNode = getAncestor(node.right);
+
+    if (leftNode && rightNode) {
+      return node;
+    } else if (leftNode && !rightNode) {
+      return leftNode;
+    } else if (!leftNode && rightNode) {
+      return rightNode;
+    } else {
+      return null;
+    }
+  };
+
+  return getAncestor(root);
 };
 // @lc code=end
