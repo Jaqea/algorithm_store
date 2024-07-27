28a7e2214b3ef459480af92a7b59ca80918c6ef0
Jaqea <1601676692@qq.com>
2024-07-27 14:24:28 +0800

    leetcode 33 ac

--- store/33.搜索旋转排序数组.js
+++ store/33.搜索旋转排序数组.js
@@ -11,22 +11,59 @@
  * @return {number}
  */
 var search = function (nums, target) {
+  // let left = 0,
+  //   right = nums.length - 1,
+  //   mid;
+  // while (left <= right) {
+  //   mid = Math.floor((left + right) / 2);
+  //   if (nums[mid] === target) return mid;
+  //   else if (target > nums[mid]) {
+  //     if (nums[mid] > nums[left]) left = mid + 1;
+  //     else if (target === nums[right]) return right;
+  //     else if (target > nums[right]) right = mid - 1;
+  //     else left = mid + 1;
+  //   } else if (target < nums[mid]) {
+  //     if (nums[mid] < nums[left]) right = mid - 1;
+  //     else if (target === nums[left]) return left;
+  //     else if (target > nums[left]) right = mid - 1;
+  //     else left = mid + 1;
+  //   }
+  // }
+
+  // return -1;
+
   let left = 0,
-    right = nums.length - 1,
-    mid;
+    right = nums.length - 1;
   while (left <= right) {
-    mid = Math.floor((left + right) / 2);
-    if (nums[mid] === target) return mid;
-    else if (target > nums[mid]) {
-      if (nums[mid] > nums[left]) left = mid + 1;
-      else if (target === nums[right]) return right;
-      else if (target > nums[right]) right = mid - 1;
-      else left = mid + 1;
-    } else if (target < nums[mid]) {
-      if (nums[mid] < nums[left]) right = mid - 1;
-      else if (target === nums[left]) return left;
-      else if (target > nums[left]) right = mid - 1;
-      else left = mid + 1;
+    const mid = Math.floor((left + right) / 2);
+    if (target === nums[mid]) {
+      return mid;
+    }
+    if (target > nums[mid]) {
+      if (nums[mid] > nums[left]) {
+        left = mid + 1;
+      } else {
+        if (target === nums[right]) {
+          return right;
+        } else if (target > nums[right]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      }
+    }
+    if (target < nums[mid]) {
+      if (nums[mid] < nums[right]) {
+        right = mid - 1;
+      } else {
+        if (target === nums[left]) {
+          return left;
+        } else if (target > nums[left]) {
+          right = mid - 1;
+        } else {
+          left = mid + 1;
+        }
+      }
     }
   }
 
