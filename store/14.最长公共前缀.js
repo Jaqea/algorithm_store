62e3b7a8ac50f6f657c22ad21f02b0eecc86e0fe
Jaqea <1601676692@qq.com>
2024-07-27 12:59:41 +0800

    leetcode 14 ac

--- store/14.最长公共前缀.js
+++ store/14.最长公共前缀.js
@@ -10,6 +10,22 @@
  * @return {string}
  */
 var longestCommonPrefix = function (strs) {
-  let i = 0;
+  let i, flag;
+
+  for (i = 0; i < strs[0].length; i++) {
+    flag = 1;
+    for (let j = 1; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        flag = 0;
+        break;
+      }
+    }
+
+    if (!flag) {
+      break;
+    }
+  }
+
+  return strs[0].slice(0, i);
 };
 // @lc code=end
